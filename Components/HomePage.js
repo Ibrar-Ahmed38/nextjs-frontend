4ec3a33c57094714427108f7922e4b5f58ea3f88
mohsinalisoomro <mohsinalisoomro23@gmail.com>
2021-12-12 13:32:59 +0500

    skew on home page

---
+++ Components/HomePage.js
@@ -0,0 +1,98 @@
+import Image from "next/image";
+
+function Homepage() {
+  return (
+    // <div className="w-full flex flex-wrap h-screen bg-gray-900">
+    //   <div className="flex flex-wrap">
+    //     <div className="parent_div  px-24 py-24 pt-20 md:pt-20 lg:pt-44">
+    //       <div className="mb-8 mx-auto items-center">
+    //         <h2 className="text-1xl mb-2 text-white  font-medium md:text-2xl lg:text-3xl ">
+    //           Find
+    //         </h2>
+    //         <h1 className=" text-2xl text-white  mb-4 font-bold md:text-5xl lg:text-6xl">
+    //           {" "}
+    //           Gay-Friendly male
+    //         </h1>
+    //         <h1 className="text-2xl  text-white font-bold md:text-5xl lg:text-6xl ">
+    //           Masseur near you
+    //         </h1>
+    //       </div>
+
+    //       <div className="absolute w-2/5 pt-8  h-28 rounded-md border-2 bg-gray-50 mb-20 shadow ">
+    //         <div className="search__location">
+    //           {/* <TiLocation className="serach_icon text-gray-900  ml-12 text-3xl"/> */}
+    //         </div>
+    //         <div className="absolute headings text-sm pl-32 ">
+    //           <p className="text-gray-600 font-medium mb-5">
+    //             city or postal code
+    //           </p>
+    //           <div className="">
+    //             <p className="font-medium">Cadriff, USA</p>
+    //           </div>
+    //         </div>
+    //         <div className="btn text-center ">
+    //           <button className="w-32 h-12 rounded-md ml-44 bg-red-700 md:ml-64 lg:ml-96">
+    //             Search
+    //           </button>
+    //         </div>
+    //       </div>
+    //     </div>
+
+    //     <div className="ml-36">
+    //       <Image
+    //         src="/images/rectangle.png"
+    //         alt="ss"
+    //         width={734}
+    //         height={757}
+    //       />
+    //     </div>
+    //   </div>
+    // </div>
+    <div
+      className="relative overflow-hidden bg-gray-400  w-screen"
+      style={{ height: "40rem" }}
+    >
+      <div className="absolute top-0 left-0 w-full">
+        <div className="flex justify-between ">
+          <div className="md:m-20 lg:m-20 text-white">
+            <div className="skew_container "></div>
+            <div className="absolute top-10 left-10">
+              <h1 className=" text-2xl text-white  mb-4 font-bold md:text-5xl lg:text-6xl">
+                FIND
+              </h1>
+              <h1 className=" text-2xl text-white  mb-4 font-bold md:text-5xl lg:text-6xl">
+                Gay-Friendly male
+              </h1>
+              <h1 className="text-2xl  text-white font-bold md:text-5xl lg:text-6xl ">
+                Masseur near you
+              </h1>
+              <div className="absolute w-2/5 pt-8  h-28 rounded-md border-2 bg-gray-50 mb-20 shadow ">
+                <div className="search__location">
+                  {/* <TiLocation className="serach_icon text-gray-900  ml-12 text-3xl"/> */}
+                </div>
+                <div className="absolute headings text-sm pl-32 ">
+                  <p className="text-gray-600 font-medium mb-5">
+                    city or postal code
+                  </p>
+                  <div className="">
+                    <p className="font-medium">Cadriff, USA</p>
+                  </div>
+                </div>
+                <div className="btn text-center ">
+                  <button className="w-32 h-12 rounded-md ml-44 bg-red-700 md:ml-64 lg:ml-96">
+                    Search
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="">
+            <img src="/images/rectangle.png" alt="ss" width="100%" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Homepage;
--- pages/index.js
+++ pages/index.js
@@ -1,60 +1,92 @@
-
- import Homepage from "./Homepage"
- import About from "../Components/about"
- import Services from "../Components/services"
-import Details from '../Components/details'
-import  Carousel  from "../Components/carousel"
-import Find from "../Components/Find"
-import Link from 'next/link'
-
+import Homepage from "../Components/HomePage";
+import About from "../Components/about";
+import Services from "../Components/services";
+import Details from "../Components/details";
+import Carousel from "../Components/carousel";
+import Find from "../Components/Find";
+import Link from "next/link";
 
 const index = () => {
-	
   return (
     <div>
-      	<nav className="bg-white shadow-lg">
-			<div className="max-w-6xl mx-auto px-4">
-				<div className="flex justify-between">
-					<div className="flex space-x-7">
-						<div>
-							<Link href=""><a className="flex items-center py-4 px-2">	
-								<span className="font-semibold text-3xl">Mr Masseur</span>
-							</a>
-							</Link> 
-						</div>
-					</div>
-					<div className="hidden md:flex items-center space-x-3 ">
-						<a href="" className="py-2 px-2 font-medium text-gray-500 rounded hover:bg-red-700 hover:text-white transition duration-300">Log In</a>
-						<Link href="/Signup"><a  className="py-2 px-2 font-medium w-32 text-center text-white bg-red-700 rounded hover:bg-red-900 transition duration-300">Sign Up</a></Link>
-					</div>
-				
-					<div className="md:hidden flex items-center">
-						<button className="outline-none mobile-menu-button">
-					</button>
-					</div>
-				</div>
-			</div>
-			
-			<div className="hidden mobile-menu">
-				<ul className="">
-					<li className="active"><a href="index.html" className="block text-sm px-2 py-4 text-white bg-green-500 font-semibold">Home</a></li>
-					<li><a href="#services" className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Services</a></li>
-					<li><a href="#about" className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">About</a></li>
-					<li><a href="#contact" className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Contact Us</a></li>
-				</ul>
-			</div>
-		
-		</nav>
-		 <Homepage /> 
-		   <About/> 
-		  <Services/> 
-		
-		  <Carousel /> 
-		  <Details/> 
-		{/* <Find />   */}
-		
-    </div>
-  )
-}
+      <nav className="bg-white shadow-lg">
+        <div className="max-w-6xl mx-auto px-4">
+          <div className="flex justify-between">
+            <div className="flex space-x-7">
+              <div>
+                <Link href="">
+                  <a className="flex items-center py-4 px-2">
+                    <span className="font-semibold text-3xl">Mr Masseur</span>
+                  </a>
+                </Link>
+              </div>
+            </div>
+            <div className="hidden md:flex items-center space-x-3 ">
+              <a
+                href=""
+                className="py-2 px-2 font-medium text-gray-500 rounded hover:bg-red-700 hover:text-white transition duration-300"
+              >
+                Log In
+              </a>
+              <Link href="/Signup">
+                <a className="py-2 px-2 font-medium w-32 text-center text-white bg-red-700 rounded hover:bg-red-900 transition duration-300">
+                  Sign Up
+                </a>
+              </Link>
+            </div>
 
-export default index
+            <div className="md:hidden flex items-center">
+              <button className="outline-none mobile-menu-button"></button>
+            </div>
+          </div>
+        </div>
+
+        <div className="hidden mobile-menu">
+          <ul className="">
+            <li className="active">
+              <a
+                href="index.html"
+                className="block text-sm px-2 py-4 text-white bg-green-500 font-semibold"
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                href="#services"
+                className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300"
+              >
+                Services
+              </a>
+            </li>
+            <li>
+              <a
+                href="#about"
+                className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300"
+              >
+                About
+              </a>
+            </li>
+            <li>
+              <a
+                href="#contact"
+                className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300"
+              >
+                Contact Us
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <Homepage />
+      <About />
+      <Services />
+
+      <Carousel />
+      <Details />
+      {/* <Find />   */}
+    </div>
+  );
+};
+
+export default index;
