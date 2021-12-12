17dd27fd170d58794326846baf734cd1e549b4ea
Ibrar Ahmed <50750853+ibrarahmedsoomro@users.noreply.github.com>
2021-12-12 13:33:18 +0500

    apply changes

--- Components/details.js
+++ Components/details.js
@@ -1,28 +1,51 @@
 import Image from "next/image";
-import 'tailwindcss/tailwind.css'
 
 function Details() {
-  return (
-    <div className="flex pt-44 bg-white">
-      <div className="w-4/5 h-80 b-2 items-center justify-center mx-auto shadow rounded-lg bg-gray-900 mb-8 ">
-        <div className="flex px-10 pt-14 items-start">
-          <div className="text-white font-medium">
-            <h1 className="text-5xl ">Start Advertising Today!</h1>
-            <p className="font-normal pt-6">
-              Select a Plan thats right for you business
-            </p>
-
-            <div className="pt-8">
-              <button className="text-red-700 bg-gray-100 w-40 h-14  font-bold rounded-md">Get Started</button>
+  return (<>
+    <div className="flex">
+        <div className="flex  pt-44 max-h-96 top-0 m-0 bg-white">
+            <div className="w-1/2 h-96 bg-black">
+              
             </div>
-            <div className="images">
-            <Image src="/images/design.png" alt="Somethingelse" className="" width={230} height={425} />
-          </div>
-          </div>
         </div>
+      <div className="flex-1 w-64 ...">
+        02
+      </div>
+      <div className="flex-1 w-32 ...">
+        03
       </div>
     </div>
+
+  </>
+    // <div classNameName="flex  pt-44 max-h-96 top-0 m-0 bg-white">
+    //   <div classNameName=" w-4/5 h-80 b-2 items-center justify-center mx-auto shadow rounded-lg bg-gray-900 mb-8 ">
+    //     <div classNameName="flex absolute px-10 pt-14 items-start">
+    //       <div classNameName="text-white font-medium">
+    //         <h1 classNameName=" text-5xl ">Start Advertising Today!</h1>
+    //         <p classNameName="font-normal pt-6">
+    //           Select a Plan thats right for you business
+    //         </p>
+
+    //         <div classNameName="flex pt-8">
+    //           <button classNameName="text-red-700 bg-gray-100 w-40 h-14  font-bold rounded-md">
+    //             Get Started
+    //           </button>
+    //           <div classNameName="images  -top-56 ">
+    //             <Image
+    //               classNameName="desgin__img"
+    //               src="/images/design.png"
+    //               alt="Somethingelse"
+    //               width={230}
+    //               height={393}
+
+    //             />
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   );
 }
 
-export default Details
+export default Details;
